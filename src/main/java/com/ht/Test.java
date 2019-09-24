074607b2710222a7547fc615c81ba6c6f6dab0bd
guxilaoren <guxilaoren.ht>
2019-09-24 23:33:44 +0800

    cs

--- src/main/java/com/ht/Test.java
+++ src/main/java/com/ht/Test.java
@@ -3,6 +3,6 @@ package com.ht;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hellod world");
     }
 }
